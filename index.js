83c609cf547227d9b4692e47f69f3f50669652e2
Amir <A.Miri>
2024-06-30 17:02:19 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import getPosts, { getPostsLength } from "./postController.js";
+console.log(getPosts());
+console.log(`Length is ${getPostsLength()}`);
+// const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+
+// console.log(generateRandomNumber());
+
+// console.log(`Celcius to fahrenhite: ${celciusToFahrenheit(0)}`);
